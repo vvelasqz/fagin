ff4251d952772b04013006fd553c3592b44a9679
Zebulun Arendsee <zbwrnz@gmail.com>
2016-05-28 18:44:55 -0500

    Extract genome sequence from GFF for Drosophila files

--- retrieve-test-data.sh
+++ retrieve-test-data.sh
@@ -5,10 +5,21 @@ species='ananassae erecta grimshawi melanogaster mojavensis persimilis pseudoobs
 base=ftp://ftp.flybase.net/genomes/12_species_analysis/genomes
 
 mkdir drosophila
+cd drosophila
 for s in $species
 do
     sbase=$base/Drosophila_$s/current
-    wget -O drosophila/$s.fna.gz $sbase/fasta/*all-chromosome*
-    wget -O drosophila/$s.faa.gz $sbase/fasta/*all-translation*
-    wget -O drosophila/$s.gff.gz $sbase/gff/*all-filtered*
+    gff=$s.gff
+    faa=$s.faa
+    fna=$s.fna
+    wget -O $faa.gz $sbase/fasta/*all-translation*
+    wget -O $gff.gz $sbase/gff/*all-filtered*
+    gunzip *gz
+    # These Drosophila GFF files contain the FASTA reference after the feature rows
+    # So here I will extract this FASTA data into a dedicated *.fna file and remove
+    # the sequence content from the GFF file.
+    sed -n '/^>/,$p' $gff > $fna
+    sed '/FASTA/,$d' $gff | sed '/###/d' > z
+    mv z $gff
 done
+cd ..

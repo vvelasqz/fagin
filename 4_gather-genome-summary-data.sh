ce951025429bc4fd4407ae2c643d3c2c430a0b13
Zebulun Arendsee <arendsee@iastate.edu>
2016-06-07 03:43:32 -0500

    Merged together rough genome summary script

--- 4_gather-genome-summary-data.sh
+++ 4_gather-genome-summary-data.sh
@@ -1,25 +1,43 @@
 #!/usr/bin/env bash
+set -u
 
 usage (){
-cat << EOF >&2
-For each species extract
- 1. length and positions of all N-strings
- 2. stats for each gene: #exons, length, UTR lengths, etc
-
-REQUIRES:
-    smof
-EOF
+    echo "Generate summary files for a set of genomes"
+    echo "REQUIRED ARGUMENTS"
+    echo "  -i Input directory for genome sequences"
+    echo "  -o Output directory"
     exit 0
 }
 
 # print help with no arguments
 [[ $# -eq 0 ]] && usage
 
-while getopts "h" opt; do
+while getopts "hi:o:" opt; do
     case $opt in
         h)
             usage ;;
+        i) 
+            idir=${OPTARG%/} ;;
+        o) 
+            odir=${OPTARG%/} ;;
     esac 
 done
 
-# TODO - implement
+[[ -d $odir ]] || mkdir $odir
+
+# input: set of full genomes
+scaflen=$odir/scaffold-lengths.tab
+echo -e "species\tscaffold\tlength" > $scaflen
+ls $idir/*fna | parallel "smof stat -q {} > $odir/{/}.tab "
+for j in $odir/*fna.tab
+do
+    s=${j%%.fna.tab}
+    s=`basename $s`
+    sed "s/^/$s\t/" $j
+    rm $j
+done >> $scaflen
+
+nlen=$odir/nstrings.tab
+echo -e "species\tscaffold\tstart\tstop" > $nlen
+ls $idir/*fna | parallel "smof grep -Poq --gff --gff-type {/.} 'N+' {}" |
+    awk 'BEGIN{FS="\t"; OFS="\t"} {print $3, $1, $4, $5}' >> $nlen

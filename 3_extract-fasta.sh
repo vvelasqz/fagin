9ecd9b1dcad58cfaf3aac1a0aed3c1a5e76cff27
Zebulun Arendsee <arendsee@iastate.edu>
2016-06-07 03:05:50 -0500

    Implement gff to faa script

--- 3_extract-fasta.sh
+++ 3_extract-fasta.sh
@@ -2,18 +2,19 @@
 
 usage (){
 cat << EOF >&2
+Arguments
+ -h print this help message
+
 Extract
  1. protein sequences for each species
  2. nucleotide sequences for each search interval
 
 REQUIRES:
     bedtools v2.23.0
+    emboss transseq
 EOF
     exit 0
 }
-
-# print help with no arguments
-[[ $# -eq 0 ]] && usage
 
 while getopts "h" opt; do
     case $opt in
@@ -22,5 +23,26 @@ while getopts "h" opt; do
     esac 
 done
 
-# TODO - get protein sequences from all species
+species=$(util/get-species-from-tree.R $tree)
+
+mkdir -p input/faa
+for s in $species
+do
+    cat input/gff/$s.gff |
+        awk '$3 == "CDS"' |
+        grep -v '#' |
+        sort -k3n -k9 |
+        awk 'BEGIN{FS="\t";OFS="\t"}{$3 = $9" "$7; print}' |
+        bedtools getfasta        \
+            -fi input/fna/$s.fna \
+            -bed /dev/stdin      \
+            -fo /dev/stdout      \
+            -name |
+        awk '$1 ~ /^>/ && $1 in seqids { next }; {seqids[$1]++; print}' |
+        smof clean -x > x
+    cat <(smof grep '+' x) <(smof grep '-' x | smof reverse -c) | 
+        transeq -filter > input/faa/$s.faa
+    rm x
+done
+
 # TODO - get nucleotide sequences for all search intervals

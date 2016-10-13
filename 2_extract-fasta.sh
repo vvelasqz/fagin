aa7395f6520215425c321613205a4c389051154b
Zebulun Arendsee <arendsee@iastate.edu>
2016-10-13 00:50:38 -0500

    Update scripts
    Scripts where synced with the new GFF parser, rid of several bugs, laden
    with new tests, and unified with a shared function file.

--- 2_extract-fasta.sh
+++ 2_extract-fasta.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 source fagin.cfg
+source src/shell-utils.sh
 
 species=$(cat $INPUT/species)
-parse_script=src/util/parse-gff.py
+PARSE_SCRIPT=$PWD/src/util/parse-gff.py
 
-usage (){
+check-exe $PARSE_SCRIPT $0
+check-exe bedtools      $0
+check-exe smof          $0
+check-exe transeq       emboss::$0
+check-exe getorf        emboss::$0
+
+usage(){
 cat << EOF >&2
 Arguments
  -h print this help message
@@ -31,16 +42,23 @@ while getopts "h" opt; do
     case $opt in
         h)
             usage ;;
-    esac 
+    esac
 done
 
 # Get transcript ORFs
 get_transcript_orfs (){
-    INPUT=$2
-    gffparser=$3
-    cat $INPUT/gff/$1.gff |
+    input_gff=$INPUT/gff/$1.gff
+    input_fna=$INPUT/fna/$1.fna
+    output_faa=$INPUT/trans-orf/$1.faa
+
+    safe-mkdir $INPUT/trans-orf
+
+    check-read  $input_gff  $0
+    check-read  $input_fna  $0
+
+    cat $input_gff |
     # select exons AND split 9th column into child and parent name columns
-    $gffparser -s exon -r ID Parent -dmp - |
+    $PARSE_SCRIPT -s exon -r ID Parent -dmp - |
     sort -k10 -k4n |
     awk '
         BEGIN{FS="\t"; OFS="\t"}
@@ -49,7 +67,7 @@ get_transcript_orfs (){
     ' |
     cut -f1-9 |
     bedtools getfasta         \
-        -fi $INPUT/fna/$1.fna \
+        -fi $input_fna        \
         -bed /dev/stdin       \
         -fo /dev/stdout       \
         -name |
@@ -58,66 +76,84 @@ get_transcript_orfs (){
         {a[$1]++; print}
     ' |
     getorf -filter -find 1 -minsize 30 |
-    smof clean -s > $INPUT/trans-orf/$1.faa
+    smof clean -s > $output_faa
 }
 
 
 # Prepare FASTA file of genes, regions potentially include UTRs and introns
 get_genes (){
-    INPUT=$2
-    gffparser=$3
-    cat $INPUT/gff/$1.gff |
-        # select mRNA and reduce 9th column to Name
-        $gffparser -s mRNA -r Name -d - |
-        bedtools getfasta         \
-            -fi $INPUT/fna/$1.fna \
-            -bed /dev/stdin       \
-            -fo /dev/stdout       \
-            -name > $INPUT/gene/$1.gene.fna
+    input_gff=$INPUT/gff/$1.gff
+    input_fna=$INPUT/fna/$1.fna
+    output_fna=$INPUT/gene/$1.gene.fna
+
+    safe-mkdir $INPUT/gene
+
+    check-read $input_gff
+    check-read $input_fna
+
+    # select mRNA and reduce 9th column to Name
+    $PARSE_SCRIPT -s mRNA -r Name -d $input_gff |
+    bedtools getfasta   \
+        -fi $input_fna  \
+        -name           \
+        -bed /dev/stdin \
+        -fo $output_fna
 }
 
 
 # Prepare protein fasta files including all predicted coding genes
 get_proteins (){
-    INPUT=$2
-    gffparser=$2
-    x=/tmp/get_proteins_x$4
-    cat $INPUT/gff/$1.gff |
+    input_gff=$INPUT/gff/$1.gff
+    input_fna=$INPUT/fna/$1.fna
+    output_faa=$INPUT/faa/$1.faa
+    x=/tmp/get_proteins_$2
+
+    safe-mkdir $INPUT/faa
+
+    check-read $input_gff
+    check-read $input_fna
+
+    cat $input_gff |
         # select CDS and reduce 9th column to Parent name
-        $gffparser -s CDS -r Parent -m - |
+        $PARSE_SCRIPT -s CDS -r Parent -m - |
         sort -k3n -k9 |
         awk '
             BEGIN{FS="\t";OFS="\t"}
             {$3 = $9" "$7; print}
         ' |
         bedtools getfasta        \
-            -fi $INPUT/fna/$1.fna \
+            -fi $input_fna \
             -bed /dev/stdin      \
             -fo /dev/stdout      \
             -name |
         awk '$1 ~ /^>/ && $1 in seqids { next }; {seqids[$1]++; print}' > $x
-    cat <(smof grep ' +' $x) <(smof grep ' -' $x | revseq -filter) | 
+    cat <(smof grep ' +' $x) <(smof grep ' -' $x | revseq -filter) |
         transeq -filter |
         sed '/>/s/_[0-9]\+//' |
-        smof clean -sux > $INPUT/faa/$1.faa
+        smof clean -sux > $output_faa
     rm $x
 }
 
 # Get open reading frames from each input genome
 get_all_orfs (){
-    INPUT=$2
-    fna=$INPUT/fna/$1.fna
-    faa=$INPUT/orf-faa/$1.faa
-    gff=$INPUT/orf-gff/$1.gff
-    smof clean --reduce-header $fna |
+    input_fna=$INPUT/fna/$1.fna
+    output_faa=$INPUT/orf-faa/$1.faa
+    output_gff=$INPUT/orf-gff/$1.gff
+
+    safe-mkdir $INPUT/orf-faa
+    safe-mkdir $INPUT/orf-gff
+
+    check-read $input_fna $0
+
+    smof clean --reduce-header $input_fna |
        # Find all START STOP bound ORFs with 10+ AA
         getorf -filter -find 1 -minsize 30 |
         # Filter out all ORFs with unknown residues
-        smof grep -v -q X | 
+        smof grep -v -q X |
         # Pipe the protein sequence to a protein fasta file
-        tee  $faa |
+        tee  $output_faa |
         # Parse a header such as:
-        # >scaffold_1_432765 [258 - 70] (REVERSE SENSE) 
+        # >scaffold_1_432765 [258 - 70] (REVERSE SENSE)
         sed -nr '/^>/ s/>([^ ]+)_([0-9])+ \[([0-9]+) - ([0-9]+)\]/\1 \3 \4 \1_\2/p' |
         # Prepare GFF
         awk '
@@ -131,22 +167,16 @@ get_all_orfs (){
                     strand = "+"
                 }
                 if($2 < $3){
-                    start = $2 
+                    start = $2
                     stop  = $3
                 } else {
-                    start = $3 
+                    start = $3
                     stop  = $2
                 }
             }
             { print seq_name, ".", "ORF", start, stop, ".", strand, ".", uid }
-        ' > $gff
+        ' > $output_gff
 }
-
-mkdir -p $INPUT/faa
-mkdir -p $INPUT/gene
-mkdir -p $INPUT/trans-orf
-mkdir -p $INPUT/orf-faa
-mkdir -p $INPUT/orf-gff
 
 # All functions are variables used in them must be exported in order for GNU
 # parallel to find them
@@ -154,9 +184,11 @@ export -f get_transcript_orfs
 export -f get_genes
 export -f get_proteins
 export -f get_all_orfs
-export $INPUT
+export -f safe-mkdir check-read 
+export PARSE_SCRIPT=$PARSE_SCRIPT
+export INPUT=$INPUT
 
-parallel "get_transcript_orfs {} $INPUT  $parse_script     " ::: $species
-parallel "get_genes           {} $INPUT  $parse_script     " ::: $species
-parallel "get_proteins        {} $INPUT  $parse_script {#} " ::: $species
-parallel "get_all_orfs        {} $INPUT                    " ::: $species
+parallel "get_transcript_orfs {}     " ::: $species
+parallel "get_genes           {}     " ::: $species
+parallel "get_proteins        {} {#} " ::: $species
+parallel "get_all_orfs        {}     " ::: $species
---
+++ src/shell-utils.sh
@@ -0,0 +1,51 @@
+print-warning(){
+    if [[ -t 2 ]]
+    then
+        echo -e "\e[1;31mERROR: \e[0m$1"
+    else
+        echo $1
+    fi
+}
+
+croak(){
+    print-warning "$1"
+    exit 1
+}
+
+check-read(){
+    file="$1"   # the name of the test file
+    place="$2"  # file context, e.g. foo.sh::doit (for debugging)
+    if [[ ! -r "$file" ]]
+    then
+        croak "Cannot read '$file', used in $place"
+    fi
+}
+
+check-dir(){
+    dir=$1    # the name of the test file
+    place=$2  # dir context, e.g. foo.sh::doit (for debugging)
+    if [[ ! -r "$dir" ]]
+    then
+        croak "Access directory '$dir', used in $place"
+    fi
+}
+
+check-exe(){
+    exe=$1
+    place=$2
+    if [[ -z `type -P $exe` ]]
+    then
+        croak "The command '$exe' cannot be executed, used in $place"
+    fi
+}
+
+safe-mkdir(){
+    dir=$1
+    [[ -d $dir ]] || mkdir -p $dir || croak "Could not make directory '$dir'"
+}
+
+kill-mkdir(){
+    dir=$1
+    [[ -d $dir ]] && rm -rf $dir
+    mkdir $dir
+}
